67434b87cdc941f4165caeb2da2a2d997470e6a3
Brujit1loquillo <jperezribelles@gamil.com>
2025-11-14 21:28:00 +0100

    saveFile.js

---
+++ backend/functions/saveFile.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+const timeStamp = require("console");
+
+const sState = require("./../functions/sessionState");
+
+module.exports = function saveFile (evName, data) {
+    // Directorio
+    const dir = sState.currentFolder;
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir, {recursive: true});
+
+    // Fichero
+    const file = path.join(dir, `${String(sState.index).padStart(6, "0")}.json`);
+
+    // Contenido
+    let allData = {
+        timestamp: new Date().toISOString(),
+        event: evName,
+        data
+    }
+
+    // Guardado
+    fs.writeFileSync(
+        file,
+        JSON.stringify(
+            allData, (_, v) => (typeof v === "bigint" ? v.toString() : v),
+            2
+        ),
+        "utf8"
+    );
+}
