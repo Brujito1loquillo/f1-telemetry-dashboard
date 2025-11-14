f36c056e6da8a90bb51b9f4db8e1ddd84a05735d
Brujit1loquillo <jperezribelles@gamil.com>
2025-11-14 23:15:55 +0100

    sDB from hSession using compareJsoon

--- backend/handlers/hSession.js
+++ backend/handlers/hSession.js
@@ -4,7 +4,10 @@ const path = require("path");
 const sState = require("./../functions/sessionState");
 const sFile = require("./../functions/saveFile");
 
-module.exports = function hSession (data) {
+const cJSON = require("./../functions/compareJson");
+const sDB = require("./../functions/saveDB");
+
+module.exports = async function hSession (data) {
     const uid = data.m_header.m_sessionUID.toString();
     if (sState.uid !== uid && uid > 0) {
         sState.currentFolder = path.join(__dirname, "..", "event", uid);
@@ -13,5 +16,21 @@ module.exports = function hSession (data) {
     }
 
     sState.index++;
-    sFile("session", data);
+
+    if (cJSON("sessionExivition1_000007.json", data)) {
+        const sesiones = await sDB("select * from sesion where uid = ?", [data.m_header.m_sessionUID]);
+        if (sesiones.length === 0) {
+            const circuitos = await sDB("select * from circuito where id = ?", [data.m_trackId]);
+            if (circuitos.length === 0) {
+                await sDB("insert into circuito (id, longitud, velocidadPitLine) values (?, ?, ?)", [data.m_trackId, data.m_trackLength, data.m_pitSpeedLimit]);
+            }
+
+            await sDB("insert into sesion (uid, tipo, circuitoId, cocheJugadorId) values (?, ?, ?, ?)", [data.m_header.m_sessionUID, data.m_sessionType, data.m_trackId, data.m_header.m_playerCarIndex]);
+        }
+
+        sFile("DB - session", data);
+    } else {
+        sFile("session", data);
+    }
+    
 }
