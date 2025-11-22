ba07463c4448cf083266b9e4033b7a01292acb56
Brujit1loquillo <jperezribelles@gamil.com>
2025-11-22 16:22:03 +0100

    Pruebas de guardado de participantes en DB

--- backend/handlers/hParticipants.js
+++ backend/handlers/hParticipants.js
@@ -4,7 +4,10 @@ const path = require("path");
 const sState = require("./../functions/sessionState");
 const sFile = require("./../functions/saveFile");
 
-module.exports = function hParticipants (data) {
+const cJSON = require("./../functions/compareJson");
+const sDB = require("./../functions/saveDB");
+
+module.exports = async function hParticipants (data) {
     const uid = data.m_header.m_sessionUID.toString();
     if (sState.uid !== uid && uid > 0) {
         sState.currentFolder = path.join(__dirname, "..", "event", uid);
@@ -13,5 +16,49 @@ module.exports = function hParticipants (data) {
     }
 
     sState.index++;
-    sFile("participants", data);
+
+    if (cJSON("participantsSigleplayerGrandPrix1_000009", data)) {
+        const playerCarIndex = data.m_header.m_playerCarIndex;
+
+        for (i = 0; i < data.m_participants.length; i++) {
+            const participant = data.m_participants[i];
+            const pilotos = await sDB("select * from piloto where nombre = ?", [participant.m_name]);
+            if (pilotos.length === 0) {
+                try {
+                    await sDB("insert into piloto values (?, ?, ?, ?, ?)", [participant.m_name, participant.m_raceNumber, participant.m_nationality, participant.m_techLevel, participant.m_platform]);
+                } catch (errPiloto) {
+                    if (errPiloto.code !== "ER_DUP_ENTRY") {
+                        console.error("Error en la insercion de piloto y no es por duplicidad.");
+                        throw errPiloto;
+                    }
+                }
+            }
+
+            const participantes = await sDB("select * from sesionParticipaPiloto where sesionUid = ? and pilotoNombre = ?", [uid, participant.m_name]);
+            if (participantes.length === 0) {
+                try {
+                    await sDB("insert into sesionParticipaPiloto values (?, ?, ?, ?, ?, ?, ?, ?)", [uid, participant.m_name, participant.m_teamId, i, participant.m_aiControlled, participant.m_yourTelemetry, participant.m_myTeam, participant.m_techLevel])
+                    // Comprovar IA:
+                        // JSON : 1 = true
+                        // DB : 1 = ?
+                    // COMPROVAR MyTELEMETRI
+                        // JSON : 1 = false
+                        // DB : 1 = ?
+                    // COMPROVAR MyTELEMETRI
+                        // JSON : 0 = false
+                        // DB : 0 = ?
+                    // OJO lvlTecnico
+                } catch (errParticipante) {
+                    if (errParticipante.code !== "ER_DUP_ENTRY") {
+                        console.error("Error en la insercion de participante y no es por duplicidad.");
+                        throw errParticipante;
+                    }
+                }
+            }
+        }
+
+        sFile("DB - participants");
+    } else {
+        sFile("participants", data);
+    }
 }
--- databases/consultas_rndm.sql
+++ databases/consultas_rndm.sql
@@ -11,13 +11,13 @@ update circuito set nombre = "Barcelona-Catalunya / Montmelo", pais = "España" 
 update circuito set nombre = "Hungria", pais = "Hungria" where id = 9;
 update circuito set nombre = "Monza", pais = "Italia" where id = 11;
 update circuito set nombre = "Interlagos / Sao Paulo", pais = "Brasil" where id = 16;
--- delete from circuito where id = 17; -- long: 4323 pit: 80 --  Ni idea de que circuito es
+update circuito set nombre = "Red Bull Ring", pais = "Austria" where id = 17;
 update circuito set nombre = "Autodromo internacional Hermanos Rodriguez", pais = "Mexico" where id = 19;
 -- update circuito set nombre = "Azerbayan", pais = "Azerbayan" where id = 20; -- pasar al fichero de creacion de la db
 update circuito set nombre = "Arabia Saudi", pais = "Arabia Saudi" where id = 29;
 update circuito set nombre = "Miami", pais = "Estados Unidos" where id = 30;
 update circuito set nombre = "Catar", pais = "Catar" where id = 32;
-update circuito set nombre = "Austria Inverso", pais = "Austria" where id = 40;
+update circuito set nombre = "Red Bull Ring Inverso", pais = "Austria" where id = 40;
 update circuito set nombre = "Zamboort Inverso", pais = "Holanda" where id = 41;
 select * from circuito;
 select * from circuito order by pais;
@@ -48,6 +48,7 @@ update equipo set nombre = "McLaren" where id = 8; -- CONFIRMADO
 update equipo set nombre = "Sauber" where id = 9; -- CONFIRMADO
 update equipo set nombre = "F1 World Generic" where id = 104; -- CONFIRAMDO
 update equipo set nombre = "Campos 25" where id = 210; -- CONFIRMADO
+update equipo set nombre = "MP 25" where id = 215; -- CONFIRMADO;
 update equipo set nombre = "Invicata 25" where id = 219; -- CONFIRMADO;
 -- update equipo set nombre = "Rendimiento equivalente" where id = 1000;
 select * from equipo;
@@ -56,14 +57,41 @@ select * from equipo where id not in (
 	0, 1000
 );
 
+/* UPDATES RAPIDAS */
+update circuito set nombre = "Suzuka", pais = "Japon" where id = 13;
+update equipo set nombre = "Hytech 25" where id = 214;
+update equipo set nombre = "Prema 25" where id = 216;
+update equipo set nombre = "Dams 25" where id = 213;
+update circuito set nombre = "Imola", pais = "Italia" where id = 27;
+update equipo set nombre = "ART 25" where id = 209;
+update equipo set nombre = "Rodin 25" where id = 211;
+update equipo set nombre = "AIX 25" where id = 212;
+update equipo set nombre = "Trident 25" where id = 217;
+update equipo set nombre = "VAR 25" where id = 218;
+update equipo set nombre = "ART 24" where id = 158;
+update equipo set nombre = "Prema 24" where id = 165;
+update equipo set nombre = "Rodin 24" where id = 160;
+update equipo set nombre = "Dams 24" where id = 162;
+update equipo set nombre = "Invicta 24" where id = 168;
+update equipo set nombre = "MP 24" where id = 164;
+update equipo set nombre = "VAR 24" where id = 167;
+update equipo set nombre = "Hitech 24" where id = 163;
+update equipo set nombre = "Campos 24" where id = 159;
+update equipo set nombre = "Trident 24" where id = 166;
+update equipo set nombre = "PHM -> AIX 24" where id = 161;
+insert into equipo values (129, "Konnersport");
+
 /* CONSULTAS RAPIDAS */
-select * from circuito;
-select * from circuito order by pais;
+-- select * from circuito;
+-- select * from circuito order by pais;
+select * from circuito where pais is not null;
+select * from circuito where pais is null;
 select * from equipo;
 select * from equipo where id > 9;
-select * from sesion;
+select * from equipo where id > 104 and id < 209;
+-- select * from sesion;
 select circuitoId, uid from sesion where circuitoId not in (
-	0, 2, 3, 4, 9, 11, 16, 29, 30, 31, 32, 40, 41
+	0, 2, 3, 4, 9, 11, 13, 15, 16, 17, 19, 27, 29, 30, 31, 32, 40, 41
 ) and uid not in (
 	13230788898168738931,15929351939921892105,1678760252547137362,6778675355917430559,7037039976247099510,16255041336025846454,891231099412765074,206017810398958312,1864810291842021278,16509879377027740714,17445207283850056036,
     12199349439267909203,4710681267069632524,14463937074756350810,10209459912444528057,14651950656740999228
@@ -73,3 +101,11 @@ select circuitoId, uid from sesion where circuitoId not in (
  * CONSULTAS PARA EL GUARDADO DE LAS VUELTAS *
  * ***************************************** */
 
+/* STORE PARTICIPANTS */
+show tables;
+select * from sesion;
+describe sesion;
+select * from sesionParticipaPiloto;
+describe sesionParticipaPiloto;
+select * from piloto;
+describe piloto;
